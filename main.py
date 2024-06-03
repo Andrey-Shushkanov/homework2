fb09b3f420bf92770738348460d8cf1711058d99
Andrey-Shushkanov <shushkanovandrey@mail.ru>
2024-06-03 18:55:57 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+ready_homeworks = 12
+quantity_of_hours = 1.5
+cours = "Python"
+time_of_one_task = quantity_of_hours / ready_homeworks
+print ("Курс: ", cours, ", всего задач: ", ready_homeworks, ", затрачено часов: ",quantity_of_hours, ", среднее время выполнения: ", time_of_one_task, " часа", sep = '')
